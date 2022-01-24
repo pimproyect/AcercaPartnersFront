667ad40635c9a7329c64f2bfcb5aa0ad75bd8af1
pimproyect <pimproyect@gmail.com>
2022-01-24 17:29:03 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddPedido from './components/addpedidos.component'
+import PedidosList from './components/pedidoslist.component'
+import Pedido from './components/pedido.component'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/pedidos"} className="nav-link">
+                Pedidos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Añadir
+              </Link>
+            </li>
+          </div>
+        </nav>
+ 
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/pedidos"]} component={PedidosList} />
+            <Route exact path="/add" component={AddPedido} />
+            <Route path="/pedidos/:Pedido" component={Pedido} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
